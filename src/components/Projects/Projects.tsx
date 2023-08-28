c67b390ab278556e741ea625119d0bf1bed73b5a
martinpanov <martin.panov98@gmail.com>
2023-08-28 13:44:40 +0300

    BEM CSS for skills and projects section, images for projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,32 +2,36 @@ import './Projects.css';
 
 export default function Projects() {
     return (
-        <section id="how-does-it-work">
-            <div className="wrapper">
-                <div className="how-does-it-work-titles">
-                    <h2 className="section--title">Projects</h2>
-                    <h3 className="section--subtitle">Showcasing Some Of My Web Development Projects.</h3>
+        <>
+            <section id="projects">
+                <div className="projects__titles">
+                    <h2 className="projects__title section--title">Projects</h2>
+                    <h3 className="projects__subtitle section--subtitle">Showcasing Some Of My Web Development Projects.</h3>
                 </div>
-                <div className="how-does-it-work-container">
-                    <img src="./images/how-does-it-work-first-image.svg" alt="how-does-it-work" />
-                    <div className="how-does-it-work-container-info">
-                        <h3 className="how-does-it-work-title">Car Marketplace & Rental</h3>
-                        <p className="how-does-it-work-paragraph">A web application which allows you to list your car for sale and rent a car.
+                <div className="projects__project-container">
+                    <div className="projects__image-wrapper first-wrapper">
+                        <img className="projects__image" src="./begachka.png" alt="begachka" />
+                    </div>
+                    <div className="projects__info">
+                        <h3 className="projects__project-title section--title">Car Marketplace & Rental</h3>
+                        <p className="projects__description">A web application which allows you to list your car for sale and rent a car.
                             The application features user authentication, with the frontend making use of a variety of HTTP methods,
                             while the backend effectively covers all CRUD operations.</p>
                     </div>
                 </div>
-                <div className="how-does-it-work-container">
-                    <img src="./images/how-does-it-work-second-image.svg" alt="how-does-it-work" />
-                    <div className="how-does-it-work-container-info">
-                        <h3 className="how-does-it-work-title">Barbershop</h3>
-                        <p className="how-does-it-work-paragraph">A website which showcases services, barbers' profiles, gallery,
+                <div className="projects__project-container">
+                    <div className="projects__image-wrapper second-wrapper">
+                        <img className="projects__image" src="./barbershop.png" alt="barbershop" />
+                    </div>
+                    <div className="projects__info">
+                        <h3 className="projects__project-title section--title">Barbershop</h3>
+                        <p className="projects__description">A website which showcases services, barbers' profiles, gallery,
                             and booking functionality. The website uses server side rendering for initial load speed and SEO,
                             along with client-side rendering for interactive user experiences, enhancing overall performance.
                         </p>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 }
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -2,55 +2,53 @@ import './Skills.css';
 
 export default function Skills() {
     return (
-        <section id="features">
-            <div className="wrapper">
-                <h2 className="section--title">Skills</h2>
-                <h3 className="section--subtitle">Our landing page template works on all devices, so you only have
-                    to set.
-                </h3>
-                <div className="features-links">
-                    <a className="feature-link" href="">
-                        <i className="fa-brands fa-react icon"></i>
-                        <h4 className="feature-title title">React</h4>
-                        <p className="feature-paragraph">Proficient in React with experience gained by developing interactive and dynamic
-                            user interfaces using components, state management, hooks, and context.</p>
-                    </a>
-                    <a className="feature-link" href="">
-                        <i className="fa-brands fa-square-js icon"></i>
-                        <h4 className="feature-title title">JavaScript</h4>
-                        <p className="feature-paragraph">During my journey of studying JavaScript, I embraced the challenge of solving different algorithmic problems.
-                            This experience developed my logical thinking and problem-solving skills.</p>
-                    </a>
-                    <a className="feature-link" href="">
-                        <i className="fa-brands fa-html5 icon"></i>
-                        <h4 className="feature-title title">HTML</h4>
-                        <p className="feature-paragraph">Experienced in writing HTML that prioritizes semantics, accessibility, and SEO,
-                            contributing to improved content quality and enhanced user engagement.</p>
-                    </a>
-                    <a className="feature-link" href="">
-                        <i className="fa-brands fa-css3-alt icon"></i>
-                        <h4 className="feature-title title">CSS</h4>
-                        <p className="feature-paragraph">Skilled at using CSS to create visually appealing and user-friendly web interfaces,
-                            combining design and functionality seamlessly.</p>
-                    </a>
-                    <a className="feature-link" href="">
-                        <i className="fa-brands fa-node-js icon"></i>
-                        <h4 className="feature-title title">NodeJS</h4>
-                        <p className="feature-paragraph">A pseudo-Latin text used in web design, layout, and printing in
-                            place
-                            of things to emphasise
-                            layouts for previewing layouts and visual mockups.</p>
-                    </a>
-                    <a className="feature-link" href="">
-                        <img className='skill-mongo-leaf icon' src="./mongo-leaf.svg" alt="MongoDB" />
-                        <h4 className="feature-title title">MongoDB</h4>
-                        <p className="feature-paragraph">A pseudo-Latin text used in web design, layout, and printing in
-                            place
-                            of things to emphasise
-                            layouts for previewing layouts and visual mockups.</p>
-                    </a>
-                </div>
-            </div>
+        <section id="skills">
+            <h2 className="skills__title section--title">Skills</h2>
+            <h3 className="skills__subtitle section--subtitle">Our landing page template works on all devices, so you only have
+                to set.
+            </h3>
+            <ul className="skills__list-items" role='list'>
+                <li className="skills__list-item">
+                    <i className="fa-brands fa-react skill--icon"></i>
+                    <h4 className="skills__skill-title section--subtitle">React</h4>
+                    <p className="skills__skill-description">Proficient in React with experience gained by developing interactive and dynamic
+                        user interfaces using components, state management, hooks, and context.</p>
+                </li>
+                <li className="skills__list-item">
+                    <i className="fa-brands fa-square-js skill--icon"></i>
+                    <h4 className="skills__skill-title section--subtitle">JavaScript</h4>
+                    <p className="skills__skill-description">During my journey of studying JavaScript, I embraced the challenge of solving different algorithmic problems.
+                        This experience developed my logical thinking and problem-solving skills.</p>
+                </li>
+                <li className="skills__list-item">
+                    <i className="fa-brands fa-html5 skill--icon"></i>
+                    <h4 className="skills__skill-title section--subtitle">HTML</h4>
+                    <p className="skills__skill-description">Experienced in writing HTML that prioritizes semantics, accessibility, and SEO,
+                        contributing to improved content quality and enhanced user engagement.</p>
+                </li>
+                <li className="skills__list-item">
+                    <i className="fa-brands fa-css3-alt skill--icon"></i>
+                    <h4 className="skills__skill-title section--subtitle">CSS</h4>
+                    <p className="skills__skill-description">Skilled at using CSS to create visually appealing and user-friendly web interfaces,
+                        combining design and functionality seamlessly.</p>
+                </li>
+                <li className="skills__list-item">
+                    <i className="fa-brands fa-node-js skill--icon"></i>
+                    <h4 className="skills__skill-title section--subtitle">NodeJS</h4>
+                    <p className="skills__skill-description">A pseudo-Latin text used in web design, layout, and printing in
+                        place
+                        of things to emphasise
+                        layouts for previewing layouts and visual mockups.</p>
+                </li>
+                <li className="skills__list-item">
+                    <img className="skill-mongo-leaf skill--icon" src="./mongo-leaf.svg" alt="MongoDB" />
+                    <h4 className="skills__skill-title section--subtitle">MongoDB</h4>
+                    <p className="skills__skill-description">A pseudo-Latin text used in web design, layout, and printing in
+                        place
+                        of things to emphasise
+                        layouts for previewing layouts and visual mockups.</p>
+                </li>
+            </ul>
         </section>
     );
 }
