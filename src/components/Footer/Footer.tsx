8480e290ffaa0d658f8adde4122d564447393db0
martinpanov <martin.panov98@gmail.com>
2023-08-30 22:55:19 +0300

    updated footer and favicon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,15 +3,7 @@ import './Footer.css';
 export default function Footer() {
     return (
         <footer id="footer">
-            <div className="footer__wrapper">
-                <ul className="footer__social-media" role="list">
-                    <li><a className="footer__social-media-links" href=""><i className="fa-brands fa-facebook-f"></i></a></li>
-                    <li><a className="footer__social-media-links" href=""><i className="fa-brands fa-twitter"></i></a></li>
-                    <li><a className="footer__social-media-links" href=""><i className="fa-brands fa-instagram"></i></a></li>
-                    <li><a className="footer__social-media-links" href=""><i className="fa-brands fa-linkedin-in"></i></a></li>
-                </ul>
-                <span className="footer__rights">2023 All right reserved</span>
-            </div>
+            Martin Panov &copy; 2023
         </footer>
     );
 }
